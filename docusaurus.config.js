dfb39f619e021f8466dd4dee192246e4e59d596a
Gene Hoffman <hoffmang@hoffmang.com>
2020-08-05 19:09:38 -0700

    Initial docusaurus commit

---
+++ docusaurus.config.js
@@ -0,0 +1,102 @@
+module.exports = {
+  title: 'Chialisp',
+  tagline: 'Developing with Chialisp',
+  url: 'https://chialisp.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  favicon: 'img/favicon.ico',
+  organizationName: 'Chia-Network', // Usually your GitHub org/user name.
+  projectName: 'chialisp-web', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Chialisp',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+  //      {to: 'blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/Chia-Network/chialisp-web',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2/',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Keybase',
+              href: 'https://keybase.io/team/chia_network.public',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/chia_project',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Chia Network Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          // It is recommended to set document id as docs home page (`docs/` path).
+          homePageId: 'doc1',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/Chia-Network/chialisp-web/edit/master/',
+        },
+//        blog: {
+//          showReadingTime: true,
+//          // Please change this to your repo.
+//          editUrl:
+//            'https://github.com/Chia-Network/chialisp-web/edit/master/blog/',
+//        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
---
+++ src/pages/index.md
@@ -0,0 +1,43 @@
+# Chialisp
+
+A LISP-like language for encumbering and releasing funds with smart-contract capabilities. Documentation is very new and some components are still rapidly changing, but this is a consolidated place to learn about Chialisp and clvm.
+
+## Introductory material
+
+- Bram’s introductory post on [Chialisp](https://www.chia.net/2019/11/27/chialisp.en.html)
+- Bram’s introduction to our [MVP of Coloured coins](https://www.chia.net/2020/04/29/coloured-coins-launch.en.html)
+- [Download Chialisp 0.4 - .tgz](https://github.com/Chia-Network/clvm/archive/0.4.tar.gz)
+
+
+## Coloured coins
+
+The first Chialisp smart transaction that we integrated into chia-blockchain were Coloured Coins. We will be integrating additional smart transactions and wallets into chia-blockchain in most releases.
+
+### Video overviews of Coloured Coins:
+
+- [Non Technical Overview and Guide](https://www.youtube.com/watch?v=YOlpmCBK8zY)
+- [Technical Guide (Part 1) - Smart Transactions and ChiaLisp](https://www.youtube.com/watch?v=17pa2t_FQQM)
+- [Technical Guide (Part 2) - The Coloured Coins Contract & Offers](https://www.youtube.com/watch?v=P33gWX4WmEQ)
+
+## Developer documentation
+
+- [ChiaLisp Compiler Repository](https://github.com/Chia-Network/clvm)
+- [A video introduction to developing in Chialisp](https://www.youtube.com/watch?v=dEFLJSU87K8)
+
+1. [Part 1: CLVM Basics](https://github.com/Chia-Network/wallets/blob/master/docs/part1_basics.md)
+2. [Part 2: Coins, Spends and Wallets](https://github.com/Chia-Network/wallets/blob/master/docs/part2_transactions.md)
+3. [Part 3: Deeper into CLVM](https://github.com/Chia-Network/wallets/blob/master/docs/part3_deeperintoCLVM.md)
+4. [Part 4: The Compiler and Other Useful Information](https://github.com/Chia-Network/wallets/blob/master/docs/part4_compiler_and_tips.md)
+5. [Glossary of Chialisp terms](https://github.com/Chia-Network/wallets/blob/master/docs/glossary.md)
+
+## Reference Wallets and Smart Transactions to be integrated
+
+- [Atomic Swaps](https://github.com/Chia-Network/wallets/blob/master/docs/atomic_swaps.md)
+- [Authorized Payees](https://github.com/Chia-Network/wallets/blob/master/docs/authorised_payees.md)
+- [Multisig Wallet](https://github.com/Chia-Network/wallets/blob/master/multisig/README.org)
+- [Rate Limited Wallet](https://github.com/Chia-Network/wallets/blob/master/docs/rate_limited_wallet.md)
+- [Recoverable Wallets](https://github.com/Chia-Network/wallets/blob/master/docs/recoverable_wallet.md)
+
+## Reference Wallets and Smart Transactions under construction
+
+- Distributed Identity Wallet - a Recoverable Wallet designed to support [DIDs](https://www.w3.org/2019/did-wg/)
