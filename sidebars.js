d550546691848240548efd1d81054c5131244c5c
Gene Hoffman <hoffmang@hoffmang.com>
2020-08-07 14:16:39 -0700

    from Docusaur to Chialisp

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3', 'doc4', 'doc5'],
+    Chialisp: ['doc1', 'doc2', 'doc3', 'doc4', 'doc5'],
 //    Features: ['mdx'],
   },
 };
