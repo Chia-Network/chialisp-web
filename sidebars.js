77851da09c604f65ca01533f99f0b5b317d7f8c1
Roy Natian <roynatian@gmail.com>
2021-08-07 19:11:09 -0400

    changed page order for tutorials

--- sidebars.js
+++ sidebars.js
@@ -16,9 +16,9 @@ module.exports = {
     CLVM: ['ref/clvm', 'ref/serialization'],
     Puzzles: ['puzzles/singletons', 'puzzles/pooling'],
     Tutorials: [
+      'tutorials/developing_applications',
       'tutorials/tools_and_setup',
       'tutorials/coin_lifecycle_and_testing',
-      'tutorials/developing_applications',
       'tutorials/singletons',
       'tutorials/custom_puzzle_lock',
       'tutorials/coin_spend_rpc'
