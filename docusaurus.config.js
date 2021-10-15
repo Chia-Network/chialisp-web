845b860c2c9df451fa9035f91d8d25abb97f8aba
Christopher Phan <phan.christopher@yahoo.com>
2021-10-15 14:41:23 -0700

    feat: added searchbar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -119,4 +119,21 @@ module.exports = {
       },
     ],
   ],
+  plugins: [
+    // ... Your other plugins.
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      {
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        // For Docs using Chinese, The `language` is recommended to set to:
+        // will add zh in when translations are ready
+        // ```
+        language: ["en"],
+        // ```
+        // When applying `zh` in language, please install `nodejieba` in your project.
+      },
+    ],
+  ],
 };
