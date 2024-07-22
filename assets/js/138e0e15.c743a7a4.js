ce0f4d2c6c56b16828e12ce06be098452d582e1d
ChiaAutomation <automation@chia.net>
2024-07-22 22:55:12 +0000

    Deploy website - based on

---
+++ assets/js/138e0e15.c743a7a4.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkchialisp_web=self.webpackChunkchialisp_web||[]).push([[524],{536:s=>{s.exports=JSON.parse('{"name":"@easyops-cn/docusaurus-search-local","id":"default"}')}}]);
--- js/matomo.js
+++ js/matomo.js
@@ -1,11 +1,1 @@
-  var _paq = window._paq = window._paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-	var u="//matomo.chia.net/";
-	_paq.push(['setTrackerUrl', u+'matomo.php']);
-	_paq.push(['setSiteId', '3']);
-	var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-	g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-  })();
+var _paq=window._paq=window._paq||[];_paq.push(["trackPageView"]),_paq.push(["enableLinkTracking"]),function(){var a="//matomo.chia.net/";_paq.push(["setTrackerUrl",a+"matomo.php"]),_paq.push(["setSiteId","3"]);var e=document,t=e.createElement("script"),p=e.getElementsByTagName("script")[0];t.async=!0,t.src=a+"matomo.js",p.parentNode.insertBefore(t,p)}();
